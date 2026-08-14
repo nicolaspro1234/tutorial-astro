29267b9b29bf982059a44eaa70810f26098d0327
Nicolas Prouvost <prouvost.nicolas@gmail.com>
2026-08-14 20:49:51 +0200

    edit index and about

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -8,9 +8,9 @@ export default function Greeting({messages}) {
 
   return (
     <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
+      <h3>{greeting}! Bonne visite ! </h3>
       <button onClick={() => setGreeting(randomMessage())}>
-        New Greeting
+        Clique-mouton
       </button>
     </div>
   );
